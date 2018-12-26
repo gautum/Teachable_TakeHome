cab7e9692e784e79cf5cc0d968705d81435d5d68
Gautum <gautum.suresh13@gmail.com>
2018-12-26 11:51:56 -0500

    adding Node object and buildGrid method

--- NeighboringNodes.py
+++ NeighboringNodes.py
@@ -1,3 +1,5 @@
+import numpt as np
+from Node import Node
 
 
 class NeighboringNodes(object):
@@ -5,3 +7,19 @@ class NeighboringNodes(object):
     def __init__(self,size,debug):
         self.size = size
         self.debug = debug
+        self.grid = np.empty(shape = (self.size,self.size), dtype = object)
+
+
+
+    def buildGrid(self):
+        # index from 1 - > size
+        count = 1
+
+        for i in range self.size:
+            for j in range self.size:
+                self.grid[i][j] = Node(i,j,count)
+                count+=1
+        if self.debug == True:
+            for i in range self.size:
+                for j in range self.size:
+                    print (self.grid[i][j].getValues())
---
+++ Node.py
@@ -0,0 +1,9 @@
+class Node(object):
+    def __init__(self,x,y,i):
+        self.x = x
+        self.y = y
+        self.i = i
+
+
+    def getValues(self):
+        return self.x, self.y, self.i
